5c99fdd61ad229874e61a2af12b6e00f22d76764
Ali <alicantokdemir@gmail.com>
2025-11-08 02:56:19 +0300

    build(sst): add initial sst config to deploy on aws

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  S3Client,
+  GetObjectCommand,
+  ListObjectsV2Command,
+} from '@aws-sdk/client-s3';
+import { Resource } from 'sst';
+import { Express } from 'express';
+import { Upload } from '@aws-sdk/lib-storage';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Post, Redirect, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiKeyAuth } from './auth/auth.decorator';
+
+const s3 = new S3Client({});
 
 @Controller()
 export class AppController {
@@ -8,5 +23,61 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiBearerAuth('api-key-auth')
+  @ApiOperation({
+    summary: 'Upload a file to S3',
+    description:
+      'Uploads a file to the configured S3 bucket. Requires API key authentication.',
+  })
+  @ApiKeyAuth()
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<string> {
+    const params = {
+      Bucket: Resource.MyBucket.name,
+      ContentType: file.mimetype,
+      Key: file.originalname,
+      Body: file.buffer,
+    };
+
+    const upload = new Upload({
+      params,
+      client: s3,
+    });
+
+    await upload.done();
+
+    return 'File uploaded successfully.';
+  }
+
+  @ApiBearerAuth('api-key-auth')
+  @ApiOperation({
+    summary: 'Get the latest file from S3',
+    description:
+      'Retrieves the latest uploaded file from the configured S3 bucket and provides a signed URL. Requires API key authentication.',
+  })
+  @ApiKeyAuth()
+  @Get('latest')
+  @Redirect('/', 302)
+  async getLatestFile() {
+    const objects = await s3.send(
+      new ListObjectsV2Command({
+        Bucket: Resource.MyBucket.name,
+      }),
+    );
+
+    const latestFile = objects.Contents.sort(
+      (a, b) => b.LastModified.getTime() - a.LastModified.getTime(),
+    )[0];
+
+    const command = new GetObjectCommand({
+      Key: latestFile.Key,
+      Bucket: Resource.MyBucket.name,
+    });
+    const url = await getSignedUrl(s3, command);
+
+    return { url };
   }
 }
