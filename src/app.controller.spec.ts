f739b6cd4f24ea8da244e8f9cffa5f5233fbd8e1
Ali <alicantokdemir@gmail.com>
2025-12-07 23:15:24 +0100

    feat(app): health and ready checks

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './loggers/logger.service';
+import { mockLoggerService } from '../test/mocks/loggerService';
+import { EntityManager } from '@mikro-orm/sqlite';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +11,27 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: LoggerService,
+          useValue: mockLoggerService,
+        },
+        {
+          provide: EntityManager,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return status ok on health', async () => {
+      expect(await appController.health()).toEqual({ status: 'ok' });
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './loggers/logger.service';
+import { EntityManager } from '@mikro-orm/sqlite';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly loggerService: LoggerService,
+    private readonly em: EntityManager,
+  ) {
+    this.loggerService.setContext(AppService.name);
+  }
+
+  async health() {
+    this.loggerService.log('Health endpoint called');
+
+    return { status: 'ok' };
+  }
+
+  async ready() {
+    this.loggerService.log('Ready endpoint called');
+    try {
+      await this.em.execute('SELECT 1');
+      return { status: 'ok' };
+    } catch (error) {
+      this.loggerService.error('Database connection failed', error);
+      return { status: 'error' };
+    }
   }
 }
